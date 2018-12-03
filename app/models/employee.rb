1d3886804ea1caec471df994c5a80603961070e6
Cess <cessmbuguar@gmail.com>
2018-12-03 09:12:25 +0300

    low-level cachng

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,25 @@
 class Employee < ApplicationRecord
-  
+  after_commit :flush_cache
+
+  VALID_GENDER = %w(male female)
+
+  scope :by_gender, -> (gender) do
+    if VALID_GENDER.include?(gender)
+      Rails.cache.fetch("employees_#{gender}") { puts 'Evaluating...' ; where gender: gender }
+    else
+      Rails.cache.fetch('all_employees') { puts 'Evaluating...' ;  all }
+    end
+  end
+
+  def final_salary
+    Rails.cache.fetch("#{cache_tax}/tax") { puts 'Calculating tax...'; salary - salary * 0.13 }
+  end
+
+  private
+
+  def flush_cache
+    puts 'Flushing cache ...'
+    Rails.cache.delete 'all_employees'
+    Rails.cache.delete "employees_ #{gender}"
+  end
 end
